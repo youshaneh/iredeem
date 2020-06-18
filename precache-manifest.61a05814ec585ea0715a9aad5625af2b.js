2f2e3c5f20edaac7eb449e3b575edbda2a1a6dd5
Shane Huang <youshaneh@gmail.com>
2020-06-18 16:55:02 +0800

    Updates

---
+++ precache-manifest.61a05814ec585ea0715a9aad5625af2b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac92aad14256f5d1278a5fcb04e4cc9d",
+    "url": "/iredeem/index.html"
+  },
+  {
+    "revision": "8c8108b92f129846f6e2",
+    "url": "/iredeem/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5ba5005625992af80ebb",
+    "url": "/iredeem/static/css/main.a21853ca.chunk.css"
+  },
+  {
+    "revision": "8c8108b92f129846f6e2",
+    "url": "/iredeem/static/js/2.bf662ab9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/iredeem/static/js/2.bf662ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba5005625992af80ebb",
+    "url": "/iredeem/static/js/main.88cefdeb.chunk.js"
+  },
+  {
+    "revision": "be8b3b20ad5184c5e624",
+    "url": "/iredeem/static/js/runtime-main.0d64c6a9.js"
+  },
+  {
+    "revision": "85a1f6392dc4370fe9a62ca05644f9bd",
+    "url": "/iredeem/static/media/about_banner.85a1f639.jpg"
+  },
+  {
+    "revision": "e82a4b8aaec6664619de84e969cda7d2",
+    "url": "/iredeem/static/media/about_bg.e82a4b8a.jpg"
+  },
+  {
+    "revision": "62686fcfc741a6e56fd301e3d894771f",
+    "url": "/iredeem/static/media/banner-1.62686fcf.jpg"
+  },
+  {
+    "revision": "170c044f45917b09dc277923b5af5eb7",
+    "url": "/iredeem/static/media/banner-2.170c044f.jpg"
+  },
+  {
+    "revision": "43b25b3c8ddf7e3f3f4dacc574b788ef",
+    "url": "/iredeem/static/media/banner_bg.43b25b3c.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/iredeem/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/iredeem/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/iredeem/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/iredeem/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/iredeem/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "46b3967bb2ed8156998625f884dcd5e9",
+    "url": "/iredeem/static/media/logo.46b3967b.png"
+  }
+]);
