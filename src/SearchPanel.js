25f2f7a509905b0ecf3fe4a7be4b0271c3242ef9
Shane Huang <youshaneh@gmail.com>
2020-06-20 14:32:13 +0800

    refactor: replace datalist with custom widgets
    to be compatible with safari on iPhone

--- src/SearchPanel.js
+++ src/SearchPanel.js
@@ -7,19 +7,9 @@ import { RouteContext } from './Contexts.js'
 
 function SearchPanel(props) {
     const { routes } = useContext(RouteContext);
-
     const [departure, setDeparture] = useState(props.departure || '');
     const [arrival, setArrival] = useState(props.arrival || '');
     const [cabin, setCabin] = useState(props.cabin || 'B');
-
-    const [timestamp, setTimestamp] = useState({});
-    if (timestamp !== props.timestamp) {
-        setTimestamp(props.timestamp);
-        setDeparture(props.departure || '');
-        setArrival(props.arrival || '');
-        setCabin(props.cabin || 'B');
-    }
-
     let departures = new Set();
     let arrivals = new Set();
     if (routes) {
@@ -28,33 +18,33 @@ function SearchPanel(props) {
             arrivals.add(route[1]);
         });
     }
-    let options = [Array.from(departures), Array.from(arrivals)]
+    let allOptions = [Array.from(departures), Array.from(arrivals)]
 
-    function isValid({ departure, arrival }) {
-        if (departure !== undefined && arrival !== undefined) throw new Error('leave either departure or arrival undefined to check if the other field is valid')
-        let fixedValue = departure ? departure : arrival;
-        let fixedIndex = departure ? 0 : 1;
-        return options[fixedIndex].includes(fixedValue);
+    function hasOption({ departure, arrival }) {
+        if (departure !== undefined && arrival !== undefined) throw new Error('leave either departure or arrival undefined to check if the value of the other field is in the option list')
+        let selectedValue = departure ? departure : arrival;
+        let selectedIndex = departure ? 0 : 1;
+        return allOptions[selectedIndex].includes(selectedValue);
     }
 
-    function getAvailableOptions({ departure, arrival }) {
-        if (departure !== undefined && arrival !== undefined) throw new Error('leave either departure or arrival undefined to get available options for the field')
-        let fixedValue = (departure !== undefined) ? departure : arrival;
-        let fixedIndex = (departure !== undefined) ? 0 : 1;
-        let optionIndex = 1 - fixedIndex;
-        return isValid({ departure, arrival }) ?
-            routes?.flatMap(route => (route[fixedIndex] === fixedValue) ? [route[optionIndex]] : [])
-            : options[optionIndex];
+    function getCorrespondingOptions({ departure, arrival }) {
+        if (!(departure !== undefined ^ arrival !== undefined)) throw new Error('leave either departure or arrival undefined to get available options for the field')
+        let selectedValue = (departure !== undefined) ? departure : arrival;
+        let selectedIndex = (selectedValue === departure) ? 0 : 1;
+        let optionIndex = 1 - selectedIndex;
+        return hasOption({ departure, arrival }) ?
+            routes?.flatMap(route => (route[selectedIndex] === selectedValue) ? [route[optionIndex]] : [])
+            : allOptions[optionIndex];
     }
 
-    let departureOptions = getAvailableOptions({ arrival });
-    let arrivalOptions = getAvailableOptions({ departure })
+    let departureOptions = getCorrespondingOptions({ arrival });
+    let arrivalOptions = getCorrespondingOptions({ departure })
 
-    let isDepartureValid = isValid({ departure }) && departureOptions.includes(departure);
-    let isArrivalValid = isValid({ arrival }) && arrivalOptions.includes(arrival);
+    let isDepartureValid = departureOptions.includes(departure);
+    let isArrivalValid = arrivalOptions.includes(arrival);
 
     const [showWarning, setShowWarning] = useState(false);
-    let history = useHistory();
+    const history = useHistory();
     function search() {
         setShowWarning(true);
         if (isDepartureValid && isArrivalValid) history.push(`/search/${departure}/${arrival}/${cabin}`);
@@ -63,52 +53,23 @@ function SearchPanel(props) {
     useEffect(() => {
         if (props.autoRefresh) search();
         // eslint-disable-next-line
-    }, [departure, arrival, cabin, props.autoRefresh]);
+    }, [departure, arrival, cabin]);
 
+    const [timestamp, setTimestamp] = useState({});
+    if (timestamp !== props.timestamp) {
+        setTimestamp(props.timestamp);
+        setDeparture(props.departure || '');
+        setArrival(props.arrival || '');
+        setCabin(props.cabin || 'B');
+    }
     return (
-        <SearchPannelArea>
-            <div className="col-md item">
-                {routes !== undefined || <Spinner className="spinner" animation="border" variant="primary" size="sm" />}
-                <input className={`wide form-control nice-select ${(showWarning && !isDepartureValid) ? 'invalid' : ''}`}
-                    placeholder={(routes !== undefined)? 'Where from?' : 'Loading...'}
-                    autoComplete="off"
-                    onFocus={e => {
-                        setDeparture('');
-                    }}
-                    list="departures" name="departure" id="departure" value={departure}
-                    onChange={e => {
-                        if(departureOptions.includes(e.target.value)) e.target.blur();
-                        setDeparture(e.target.value);
-                    }} />
-                <datalist id="departures">
-                    {departureOptions?.map(
-                        option =>
-                            <option key={option} value={option}>
-                                {airports[option].city + ' - ' + airports[option].name}
-                            </option>)}
-                </datalist>
-            </div>
-            <div className="col-md item">
-                {routes !== undefined || <Spinner className="spinner" animation="border" variant="primary" size="sm" />}
-                <input className={`wide form-control nice-select ${(showWarning && !isArrivalValid) ? 'invalid' : ''}`}
-                    placeholder={(routes !== undefined)? 'Where to?' : 'Loading...'}
-                    autoComplete="off"
-                    onFocus={e => {
-                        setArrival('');
-                    }}
-                    list="arrivals" name="arrival" id="arrival" value={arrival}
-                    onChange={e => {
-                        if(arrivalOptions.includes(e.target.value)) e.target.blur();
-                        setArrival(e.target.value);
-                    }} />
-                <datalist id="arrivals">
-                    {arrivalOptions?.map(
-                        option =>
-                            <option key={option} value={option}>
-                                {airports[option].city + ' - ' + airports[option].name}
-                            </option>)}
-                </datalist>
-            </div>
+        <SearchPannelFrame>
+            <AutoCompleteInput value={departure} routes={routes} warn={showWarning && !isDepartureValid}
+                updateFunction={setDeparture} validOptions={departureOptions} allOptions={allOptions[0]}
+                placeholder="Where from?" invalidMsg=" - Not applicable for this arrival" />
+            <AutoCompleteInput value={arrival} routes={routes} warn={showWarning && !isArrivalValid}
+                updateFunction={setArrival} validOptions={arrivalOptions} allOptions={allOptions[1]}
+                placeholder="Where to?" invalidMsg=" - Not applicable for this departure" />
             <div className="col-md item">
                 <select className="wide nice-select" value={cabin}
                     onChange={e => {
@@ -130,11 +91,55 @@ function SearchPanel(props) {
                     </div>
                 </div>
             }
-        </SearchPannelArea>
+        </SearchPannelFrame>
     )
 }
 
-function SearchPannelArea(props) {
+function AutoCompleteInput(props) {
+    const [focused, setFocused] = useState(false);
+    const allOptions = props.allOptions ? props.allOptions.filter(option => option.indexOf(props.value.toUpperCase()) >= 0) : [];
+    const validOptions = props.validOptions ? props.validOptions.filter(option => option.indexOf(props.value.toUpperCase()) >= 0) : [];
+    const invalidOptions = allOptions.flatMap(option => validOptions.includes(option) ? [] : [option]);
+    return (
+        <div className="col-md item">
+            {props.routes !== undefined || <Spinner className="spinner" animation="border" variant="primary" size="sm" />}
+            <input className={`wide form-control nice-select ${props.warn ? 'invalid' : ''}`}
+                value={props.value}
+                placeholder={(props.routes !== undefined) ? props.placeholder : 'Loading...'}
+                onFocus={() => {
+                    props.updateFunction('');
+                    setFocused(true);
+                }}
+                onBlur={() => {
+                    if (validOptions.length === 1 && props.value.length > 2) props.updateFunction(validOptions[0]);
+                    setFocused(false)
+                }}
+                onChange={e => {
+                    props.updateFunction(e.target.value);
+                    if (props.validOptions.includes(e.target.value)) e.target.blur();
+                }} />
+            <div className={`options ${focused ? '' : 'hide'}`}>
+                <div className="border">
+                    {validOptions.map(option =>
+                        <div className="airport-option" key={option}
+                            onMouseDown={() => {
+                                props.updateFunction(option);
+                            }}>
+                            <div className="airport">{option}</div>
+                            <div className="details">{airports[option].city}</div>
+                        </div>)}
+                    {invalidOptions.map(option =>
+                        <div className="airport-option invalid" key={option}>
+                            <div className="airport">{option + props.invalidMsg}</div>
+                            <div className="details">{airports[option].city}</div>
+                        </div>)}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function SearchPannelFrame(props) {
     return (
         <div className="hotel_booking_area position">
             <div className="container">
